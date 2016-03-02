086ff23fdeac888b8a345ff19d19c8f3f3ecf911
zeroplusone <a711186@gmail.com>
2016-03-02 20:20:30 +0800

    Add A.cpp for question A

---
+++ A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Please input an integer:" << endl;
+    cin >> n;
+    cout << n << " ";
+    while(n != 1){
+
+        if(n % 2 == 1)
+            n = 3 * n + 1;
+        else
+            n /= 2;
+        cout << n << " ";
+    }
+    cout << endl;
+    return 0;
+}
